4fbbd19facf91a00a6dc81ca8281616e890c5de3
codyuhi <codylkuhi@gmail.com>
2019-08-16 15:21:47 -0400

    Update athleteSort.py
    This version of athleteSort is much shorter and takes advantage of comprehensions

--- python/athleteSort.py
+++ python/athleteSort.py
@@ -1,41 +1,6 @@
-#!/bin/python3
+N, M = map(int, input().split())
+rows = [input() for _ in range(N)]
+K = int(input())
 
-import math
-import os
-import random
-import re
-import sys
-
-
-
-if __name__ == '__main__':
-    nm = input().split()
-
-    n = int(nm[0])
-
-    m = int(nm[1])
-
-    arr = []
-
-    for _ in range(n):
-        arr.append(list(map(int, input().rstrip().split())))
-
-    k = int(input())
-
-    sortedKValues = sorted(list(i[k] for i in arr))
-    # print(sortedKValues)
-    sortArr = []
-    for i in sortedKValues:
-        for j in range(len(arr)):
-            # print(j)
-            if i == arr[j][k]:
-                sortArr.append(arr[j])
-                del arr[j]
-                break
-    for i in sortArr:
-        out = ""
-        for j in range(m):
-            if j > 0:
-                out += " "
-            out += str(i[j])
-        print(out)
+for row in sorted(rows, key=lambda row: int(row.split()[K])):
+    print(row)
