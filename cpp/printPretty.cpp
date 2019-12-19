1a187803b780d5f03b779301ab5dc4845ee1e6ee
Cody Uhi <codylkuhi@gmail.com>
2019-12-19 12:36:56 -0700

    Update printPretty.cpp

--- cpp/printPretty.cpp
+++ cpp/printPretty.cpp
@@ -12,9 +12,19 @@ int main() {
 		double C; cin >> C;
 
 		/* Enter your code here */
-        cout << setprecision(0) << A << endl;
-		cout << setprecision(0) << A << endl;
-		cout << setprecision(9) << showpoint <<  C << endl;
+        
+		// LINE 1 
+        cout << hex << left << showbase << nouppercase; // formatting
+        cout << (long long) A << endl; // actual printed part
+
+        // LINE 2
+        cout << dec << right << setw(15) << setfill('_') << showpos << fixed << setprecision(2); // formatting
+        cout << B << endl; // actual printed part
+
+        // LINE 3
+        cout << scientific << uppercase << noshowpos << setprecision(9); // formatting
+        cout << C << endl; // actual printed p
+
 	}
 	return 0;
 
